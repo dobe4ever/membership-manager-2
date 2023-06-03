afe58c1affc40e12fd39ad1a22d03481f077afdf
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2023-06-03 02:31:07 +0000

    database connected & setting expired members 'active' from 1 to 0

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from database import engine
 from sqlalchemy import text
 import os
+from datetime import date
 
 token = os.environ['TOKEN']
 
@@ -8,6 +9,19 @@ with engine.connect() as conn:
     result = conn.execute(text("SELECT * FROM users"))
     rows = result.fetchall()
     rows_as_dicts = [dict(row._asdict()) for row in rows]
-    print(rows_as_dicts)
 
+    for row in rows_as_dicts:
+        expiration_date = row['expiration_date']
+        if expiration_date < date.today():
+            user_id = row['user_id']
+            conn.execute(
+                text("UPDATE users SET active = 0 WHERE user_id = :user_id"),
+                {'user_id': user_id}
+            )
+
+    result = conn.execute(text("SELECT * FROM users"))
+    rows = result.fetchall()
+    rows_as_dicts = [dict(row._asdict()) for row in rows]
+    print(rows_as_dicts)
+  
 #####
